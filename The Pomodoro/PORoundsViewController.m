5c9af71a35d71221f976ddf826a23164cd06b026
Tyler Olsen <ty_guy5@me.com>
2015-04-15 23:32:12 -0600

    Project data files

--- The Pomodoro/PORoundsViewController.m
+++ The Pomodoro/PORoundsViewController.m
@@ -8,21 +8,63 @@
 
 #import "PORoundsViewController.h"
 
-@interface PORoundsViewController ()
+static NSString *reuseID = @"reuseID";
+
+
+@interface PORoundsViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
 @implementation PORoundsViewController
 
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [self registerForNotifications];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.title = @"Rounds";
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:reuseID];
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+//- (void)dealloc
+//{
+//    [self unregisterForNotifications];
+//}
+
+#pragma mark by Tyler - TableView DataSource Methods
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[tableView dequeueReusableCellWithIdentifier:reuseID];
+                             NSArray *roundsArray = [RoundsController sharedInstance].roundsTimes;
+                             NSNumber *minutes = roundsArray[indexPath.row];
+}
+
+
+
+
 
 /*
 #pragma mark - Navigation
